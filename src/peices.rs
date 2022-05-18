2fdc17dc9781ecacf058f0d0f677d18c7c2576b8
Shane McDonough <shanemcdman@gmail.com>
2022-05-17 22:40:46 -0700

    impl ToString for PieceType

--- src/peices.rs
+++ src/peices.rs
@@ -7,6 +7,20 @@ enum PieceType {
     King
 }
 
+impl ToString for PieceType {
+    fn to_string(&self) -> String {
+        use Self::*;
+        match(self) {
+            Pawn => "p",
+            Rook => "R",
+            Knight => "K",
+            Bishop => "B",
+            Queen => "Q",
+            King => "K"
+        }
+    }
+}
+
 enum PieceTeam {
     Black,
     White
